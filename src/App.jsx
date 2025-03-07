1a8986098177df51af49ee17ac5d39afb10b1bd9
adriancancode <vazqueza2004@gmail.com>
2025-03-07 09:16:04 -0800

    Completed Flashcard functionality and CSS styling

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import { useState } from 'react';
+import Card from './components/Card.jsx';
+import flashcards from './data.js';
+import './App.css';
+
+const App = () => {
+  const [card, setCard] = useState(0);
+  const[showAnswer, setShowAnswer] = useState(false);
+  function flipBackward() {
+    setCard((prevCard) => (prevCard - 1 + flashcards.length) % flashcards.length);
+    setShowAnswer(false);
+    
+  }
+
+  function flipForward() {
+    setCard((prevCard) => (prevCard + 1) % flashcards.length);
+    setShowAnswer(false);
+    
+  }
+
+
+  return (
+    <div className='body'>
+      <div className='header'>
+        <h1>Flashcards of Rock</h1>
+        <h2>Test your inner rock n' roller with these flashcards!</h2>
+      </div>
+      <Card question={showAnswer ? flashcards[card].answer : flashcards[card].question}/>
+      <button onClick={() => setShowAnswer(!showAnswer)}>{showAnswer ? "Show Answer" : "Show Question"}</button>
+      
+      <div className='button-container'>
+        <button onClick={flipBackward}>Prev</button>
+        <button onClick={flipForward}>Next</button>
+      </div>
+    </div>
+      
+  
+  )
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,26 @@
+const flashcards = [{
+    id : 1,
+    question : "What band recorded the hit song 'Sweet Child O' Mine?'",
+    answer: "Guns N' Roses"
+},
+{
+    id: 2,
+    question : "Who is the guitarist for Queen?",
+    answer : "Brian May"
+}, {
+    id: 3,
+    question : "What year did the album 'Abbey Road' by The Beatles come out?",
+    answer : "1969"
+},
+{
+    id : 4,
+    question : "What city in America are Aerosmith from?",
+    answer : "Boston"
+},
+{
+    id : 5,
+    question : "What song have Metallica played live the most times?",
+    answer : "Master of Puppets"
+}]
+
+export default flashcards;
